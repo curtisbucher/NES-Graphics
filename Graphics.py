b93b1eb24738b57049ca17b43b227536a618ad9f
curtisbucher <cpbucher5@gmail.com>
2020-02-12 23:13:49 -0800

    show inactive pallettes, fixed crash on switching pallettes

--- Graphics.py
+++ Graphics.py
@@ -1,140 +1,164 @@
 ## Renders the background using pallette anc chr and tiles
 ## http://www.dustmop.io/blog/2015/04/28/nes-graphics-part-1/
 import pygame
-COLORS = [(124, 124, 124),
- (0, 0, 252),
- (0, 0, 188),
- (68, 40, 188),
- (148, 0, 132),
- (168, 0, 32),
- (168, 16, 0),
- (136, 20, 0),
- (80, 48, 0),
- (0, 120, 0),
- (0, 104, 0),
- (0, 88, 0),
- (0, 64, 88),
- (0, 0, 0),
- (0, 0, 0),
- (0, 0, 0),
- (188, 188, 188),
- (0, 120, 248),
- (0, 88, 248),
- (104, 68, 252),
- (216, 0, 204),
- (228, 0, 88),
- (248, 56, 0),
- (228, 92, 16),
- (172, 124, 0),
- (0, 184, 0),
- (0, 168, 0),
- (0, 168, 68),
- (0, 136, 136),
- (0, 0, 0),
- (0, 0, 0),
- (0, 0, 0),
- (248, 248, 248),
- (60, 188, 252),
- (104, 136, 252),
- (152, 120, 248),
- (248, 120, 248),
- (248, 88, 152),
- (248, 120, 88),
- (252, 160, 68),
- (248, 184, 0),
- (184, 248, 24),
- (88, 216, 84),
- (88, 248, 152),
- (0, 232, 216),
- (120, 120, 120),
- (0, 0, 0),
- (0, 0, 0),
- (252, 252, 252),
- (164, 228, 252),
- (184, 184, 248),
- (216, 184, 248),
- (248, 184, 248),
- (248, 164, 192),
- (240, 208, 176),
- (252, 224, 168),
- (248, 216, 120),
- (216, 248, 120),
- (184, 248, 184),
- (184, 248, 216),
- (0, 252, 252),
- (248, 216, 248),
- (0, 0, 0),
- (0, 0, 0)]
 
-CHR_COLORS = ((0,0,0),(128,128,128),(255,255,255))
+COLORS = [
+    (124, 124, 124),
+    (0, 0, 252),
+    (0, 0, 188),
+    (68, 40, 188),
+    (148, 0, 132),
+    (168, 0, 32),
+    (168, 16, 0),
+    (136, 20, 0),
+    (80, 48, 0),
+    (0, 120, 0),
+    (0, 104, 0),
+    (0, 88, 0),
+    (0, 64, 88),
+    (0, 0, 0),
+    (0, 0, 0),
+    (0, 0, 0),
+    (188, 188, 188),
+    (0, 120, 248),
+    (0, 88, 248),
+    (104, 68, 252),
+    (216, 0, 204),
+    (228, 0, 88),
+    (248, 56, 0),
+    (228, 92, 16),
+    (172, 124, 0),
+    (0, 184, 0),
+    (0, 168, 0),
+    (0, 168, 68),
+    (0, 136, 136),
+    (0, 0, 0),
+    (0, 0, 0),
+    (0, 0, 0),
+    (248, 248, 248),
+    (60, 188, 252),
+    (104, 136, 252),
+    (152, 120, 248),
+    (248, 120, 248),
+    (248, 88, 152),
+    (248, 120, 88),
+    (252, 160, 68),
+    (248, 184, 0),
+    (184, 248, 24),
+    (88, 216, 84),
+    (88, 248, 152),
+    (0, 232, 216),
+    (120, 120, 120),
+    (0, 0, 0),
+    (0, 0, 0),
+    (252, 252, 252),
+    (164, 228, 252),
+    (184, 184, 248),
+    (216, 184, 248),
+    (248, 184, 248),
+    (248, 164, 192),
+    (240, 208, 176),
+    (252, 224, 168),
+    (248, 216, 120),
+    (216, 248, 120),
+    (184, 248, 184),
+    (184, 248, 216),
+    (0, 252, 252),
+    (248, 216, 248),
+    (0, 0, 0),
+    (0, 0, 0),
+]
+
+CHR_COLORS = ((0, 0, 0), (128, 128, 128), (255, 255, 255))
 
 
 class Background:
     def __init__(self):
         ## Four pallette objects each with four colors, one of which is shared between the four
-        self.pallettes = [Pallette(0,1,2,3,x) for x in range(4)]
+        self.pallettes = [Pallette(0, 1, 2, 3, x) for x in range(4)]
         ## Atributes. Essentially a nametable for pallettes. 15 x 16 (240)
-        self.attributes = [0b00]*240
+        self.attributes = [0b00] * 240
         ## 256 CHR objects
         self.chr = [CHR(x) for x in range(256)]
         ## 30 x 32 (960) 8 bit numbers assigning chr files to position on the screen
-        self.nametable = [0]*960
-        
+        self.nametable = [0] * 960
+
     def to_surf(self, size):
         width, height = size
         surf = pygame.Surface((width, height))
-        chr_size = width//30, height//32
+        chr_size = width // 30, height // 32
         for y in range(32):
             for x in range(30):
-                CHR = self.chr[self.nametable[y*30+x]]
+                CHR = self.chr[self.nametable[y * 30 + x]]
                 try:
-                    surf.blit(CHR.to_surf(chr_size, self.pallettes[self.attributes[x//2 + y//2* 15]]), (x*chr_size[0],y*chr_size[1]))
+                    surf.blit(
+                        CHR.to_surf(
+                            chr_size,
+                            self.pallettes[self.attributes[x // 2 + y // 2 * 15]],
+                        ),
+                        (x * chr_size[0], y * chr_size[1]),
+                    )
                 except Exception:
                     print(self.attributes)
                     raise Exception
         return surf
-    
+
+    def pallettes_to_surf(self, size):
+        surf = pygame.Surface((2, 8))
+        for a in range(16):
+            surf.set_at((a % 2, a // 2), COLORS[self.pallettes[a // 4][a % 4]])
+        return pygame.transform.scale(surf, size)
+
     def save(self, filename):
         ## Compressing pallettes into list of 13 colors (shared background color)
         pallette_bytes = [Pallette.shared_background]
         for x in range(4):
             pallette_bytes += self.pallettes[x].to_bytes()
-            
+
         ## Compressing 2 bit attributes into list of bytes
         attribute_bytes = []
-        for x in range(0,240,4):
-            attribute_bytes += [self.attributes[x] 
-                                + self.attributes[x + 1] 
-                                + self.attributes[x + 2] 
-                                + self.attributes[x + 3]]
+        for x in range(0, 240, 4):
+            attribute_bytes += [
+                self.attributes[x]
+                + self.attributes[x + 1]
+                + self.attributes[x + 2]
+                + self.attributes[x + 3]
+            ]
         ## Nametable is already 960 x bytes
         nametable_bytes = self.nametable
-        
+
         ## Compressing 2 * 64 * 256 byte chr tiles
         chr_bytes = []
         for x in range(256):
             chr_bytes += self.chr[x].to_bytes()
-            
+
         ## Ordered by size of data
-        total_data = bytes(pallette_bytes + attribute_bytes + nametable_bytes + chr_bytes)
+        print(pallette_bytes, attribute_bytes, nametable_bytes, chr_bytes)
+        total_data = bytes(
+            pallette_bytes + attribute_bytes + nametable_bytes + chr_bytes
+        )
         with open(filename, "wb") as file:
             file.write(total_data)
-            
+
     def load(self, filename):
         with open(filename, "rb") as file:
             data = file.read()
-            
+
         ## Loading Pallette data
         self.pallettes = []
         pallette_bytes = data[0:13]
 
         background = int(pallette_bytes[0])
-        for x in range(1,13,3):
-            self.pallettes += [Pallette(background, 
-                          int(pallette_bytes[x]), 
-                          int(pallette_bytes[x+1]), 
-                          int(pallette_bytes[x+2]))]
-            
+        for x in range(1, 13, 3):
+            self.pallettes += [
+                Pallette(
+                    background,
+                    int(pallette_bytes[x]),
+                    int(pallette_bytes[x + 1]),
+                    int(pallette_bytes[x + 2]),
+                )
+            ]
+
         ## Loading attribute data
         self.attributes = []
         attribute_bytes = data[13:73]
@@ -143,22 +167,22 @@ class Background:
             self.attributes += [attribute_bytes[x] & 0b00110000]
             self.attributes += [attribute_bytes[x] & 0b00001100]
             self.attributes += [attribute_bytes[x] & 0b00001111]
-            
+
         ## Loading nametable data
         nametable_bytes = data[73:1033]
         self.nametable = [int(x) for x in nametable_bytes]
-        
+
         ## Loading chr data
         chr_bytes = data[1033:5129]
         self.chr = []
         for x in range(256):
             self.chr += [CHR(x)]
             self.chr[x].CHR = []
-            for a in range(0,16):
-                self.chr[x].CHR += [chr_bytes[x*16 - 16 + a] & 0b11000000]
-                self.chr[x].CHR += [chr_bytes[x*16 - 16 + a] & 0b00110000]
-                self.chr[x].CHR += [chr_bytes[x*16 - 16 + a] & 0b00001100]
-                self.chr[x].CHR += [chr_bytes[x*16 - 16 + a] & 0b00000011]
+            for a in range(0, 16):
+                self.chr[x].CHR += [chr_bytes[x * 16 - 16 + a] & 0b11000000]
+                self.chr[x].CHR += [chr_bytes[x * 16 - 16 + a] & 0b00110000]
+                self.chr[x].CHR += [chr_bytes[x * 16 - 16 + a] & 0b00001100]
+                self.chr[x].CHR += [chr_bytes[x * 16 - 16 + a] & 0b00000011]
 
 
 class Pallette:
@@ -174,21 +198,23 @@ class Pallette:
         self.color2 = color2
         self.color3 = color3
         self.index = index
-        
+
     def __getitem__(self, index):
         if index == 0:
-            return self.shared_background
-        if index == 1:
+            return Pallette.shared_background
+        elif index == 1:
             return self.color1
-        if index == 2:
+        elif index == 2:
             return self.color2
-        if index == 3:
+        elif index == 3:
             return self.color3
-        raise IndexError(" Pallette index out of range")
-        
+        else:
+            print(index)
+            raise IndexError(" Pallette index out of range")
+
     def __setitem__(self, index, value):
         if index == 0:
-            self.shared_background = value
+            Pallette.shared_background = value
         elif index == 1:
             self.color1 = value
         elif index == 2:
@@ -197,41 +223,40 @@ class Pallette:
             self.color3 = value
         else:
             raise IndexError(" Pallette index out of range")
-        
+
     def to_surf(self, size):
-        surf = pygame.Surface((1,4))
+        surf = pygame.Surface((1, 4))
         for y in range(4):
-            surf.set_at((0,y), COLORS[self[y]])
-        return pygame.transform.scale(surf,size)
+            surf.set_at((0, y), COLORS[self[y]])
+        return pygame.transform.scale(surf, size)
 
     def to_bytes(self):
         """ Creates list object from pallette for saving"""
         pallette_bytes = []
-        for x in range(1,4):
+        for x in range(1, 4):
             pallette_bytes += [self[x]]
         return pallette_bytes
 
 
 ## Setting up tile data
 class CHR:
-    def __init__(self, index = 0):
+    def __init__(self, index=0):
         # 8 x 8 pixels of two bit depth
-        self.CHR = [0b00]*64
+        self.CHR = [0b00] * 64
         self.index = index
-        
-    def to_surf(self,size, pallette):
-        surf = pygame.Surface((8,8))
+
+    def to_surf(self, size, pallette):
+        surf = pygame.Surface((8, 8))
         for y in range(8):
             for x in range(8):
-                surf.set_at((x,y), COLORS[pallette[self.CHR[y*8+x]]])
-        return pygame.transform.scale(surf,size)
-    
+                surf.set_at((x, y), COLORS[pallette[self.CHR[y * 8 + x]]])
+        return pygame.transform.scale(surf, size)
+
     def to_bytes(self):
         """ Compresses chr to list of bytes for saving """
         chr_bytes = []
-        for x in range(0,64,4):
-            chr_bytes += [self.CHR[x] 
-                        + self.CHR[x + 1] 
-                        + self.CHR[x + 2] 
-                        + self.CHR[x + 3]]
+        for x in range(0, 64, 4):
+            chr_bytes += [
+                self.CHR[x] + self.CHR[x + 1] + self.CHR[x + 2] + self.CHR[x + 3]
+            ]
         return chr_bytes
--- chr_generator.py
+++ chr_generator.py
@@ -20,77 +20,96 @@ background = Graphics.Background()
 chr_index = 0
 CHR = background.chr[chr_index]
 pallette_index = 0
-pallette = Graphics.Pallette(0,1,2,3)
+pallette = Graphics.Pallette(0, 1, 2, 3)
 CHR.pallette = pallette
 
-active = 0 #0 = chr, 1-4 = pallette colors. Defines which portion is being edited
+ACTIVE = 0  # 0 = chr, 1-4 = pallette colors. Defines which portion is being edited
 
 
 def update_screen():
     """ Updates the screen. Called every screen refresh"""
-    pygame.display.set_caption("CHR Index: " + str(chr_index) + "\tPallette Index: " + str(pallette_index))
-    CHR = background.chr[chr_index]
-    screen.fill((255,255,255))
-    
-    screen.blit(CHR.to_surf((512,512), background.pallettes[pallette_index]), (512,0))
-    screen.blit(background.pallettes[pallette_index].to_surf((64,256)), (1024,0))
-    screen.blit(background.to_surf((512,512)),(0,0))
-        
-    pygame.draw.line(screen, (0,0,0),(1024,0),(1024,512),3)
-    pygame.draw.line(screen, (0,0,0),(1024,256), (1088, 256),3)
-    for y in range(0,256,64):
-        pygame.draw.line(screen, (0,0,0),(1024,y), (1088, y),1)
-        
-    if not active:
-        pygame.draw.rect(screen, (255,0,0), (512,0,512,512), 2)
-    elif active == 1:
-        pygame.draw.rect(screen, (255,0,0), (1024,0,64,64), 2)
-    elif active == 2:
-        pygame.draw.rect(screen, (255,0,0), (1024,64,64,64), 2)
-    elif active == 3:
-        pygame.draw.rect(screen, (255,0,0), (1024,128,64,64), 2)
-    elif active == 4:
-        pygame.draw.rect(screen, (255,0,0), (1024,192,64,64), 2)
-    
-        
-    pygame.display.flip()
-    
+    ## Setting the Caption
+    pygame.display.set_caption(
+        "CHR Index: " + str(chr_index) + "\tPallette Index: " + str(pallette_index)
+    )
 
-def click(x,y):
+    CHR = background.chr[chr_index]
+
+    ## Blitting the background
+    screen.fill((255, 255, 255))
+
+    ## Blitting the chr that is being edited
+    screen.blit(CHR.to_surf((512, 512), background.pallettes[pallette_index]), (512, 0))
+
+    ## Blitting the active pallette
+    screen.blit(background.pallettes[pallette_index].to_surf((64, 256)), (1024, 0))
+
+    ## Blitting all the pallettes
+    screen.blit(background.pallettes_to_surf((64, 256)), (1024, 256))
+
+    ## Blitting the background
+    screen.blit(background.to_surf((512, 512)), (0, 0))
+
+    ## Drawing lines to seperate segments
+    pygame.draw.line(screen, (0, 0, 0), (1024, 0), (1024, 512), 3)
+    pygame.draw.line(screen, (0, 0, 0), (1024, 256), (1088, 256), 3)
+
+    ## Drawing lines to seperate active pallette colors and inactive pallettes
+    for y in range(0, 512, 64):
+        pygame.draw.line(screen, (0, 0, 0), (1024, y), (1088, y), 1)
+
+    if not ACTIVE:
+        pygame.draw.rect(screen, (255, 0, 0), (512, 0, 512, 512), 2)
+    elif ACTIVE == 1:
+        pygame.draw.rect(screen, (255, 0, 0), (1024, 0, 64, 64), 2)
+    elif ACTIVE == 2:
+        pygame.draw.rect(screen, (255, 0, 0), (1024, 64, 64, 64), 2)
+    elif ACTIVE == 3:
+        pygame.draw.rect(screen, (255, 0, 0), (1024, 128, 64, 64), 2)
+    elif ACTIVE == 4:
+        pygame.draw.rect(screen, (255, 0, 0), (1024, 192, 64, 64), 2)
+
+    pygame.display.flip()
+
+
+def click(x, y):
     """ Handles mouse click detection"""
-    global active
+    global ACTIVE
+
+    ## If the mouse is modifying the background portion
+    if x < 512:
+        if not ACTIVE:
+            background.nametable[x // (512 // 30) + (y // (512 // 32) * 30)] = chr_index
+        else:
+            background.attributes[
+                x // (512 // 15) + (y // (512 // 16) * 15)
+            ] = pallette_index
+
     ## If the mouse is modifying the CHR portion
     CHR = background.chr[chr_index]
-    if x < 512:
-        if not active:
-            background.nametable[x//(512//30) + (y//(512//32) *30)] = chr_index
-        else:
-            background.attributes[x//(512//15) + (y//(512//16) *15)] = pallette_index
-    ## If the mouse is modifying the CHR portion
     if x > 512 and x < 1024:
-        active = 0
-        index = (x//64 - 8) + (y//64 * 8)
+        ACTIVE = 0
+        index = (x // 64 - 8) + (y // 64 * 8)
         if CHR.CHR[index] < 3:
             CHR.CHR[index] += 1
         else:
             CHR.CHR[index] = 0
-            
+
     ## If the mouse is modifying the pallette portion
     elif x > 1024 and y > 0 and y < 64:
-        active = 1
+        ACTIVE = 1
     elif x > 1024 and y > 64 and y < 128:
-        active = 2
+        ACTIVE = 2
     elif x > 1024 and y > 128 and y < 192:
-        active = 3
+        ACTIVE = 3
     elif x > 1024 and y > 192 and y < 256:
-        active = 4
-    
-    
+        ACTIVE = 4
+
+
 done = False
 while not done:
-    update_screen()
-            
     for event in pygame.event.get():
+        update_screen()
         if event.type == pygame.QUIT:
             done = True
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -98,7 +117,7 @@ while not done:
         if event.type == pygame.KEYDOWN:
             key = pygame.key.get_pressed()
             if key[pygame.K_RIGHT]:
-                if not active:
+                if not ACTIVE:
                     if chr_index < 255:
                         chr_index += 1
                     else:
@@ -109,7 +128,7 @@ while not done:
                     else:
                         pallette_index = 0
             elif key[pygame.K_LEFT]:
-                if not active:
+                if not ACTIVE:
                     if chr_index > 0:
                         chr_index -= 1
                     else:
@@ -119,23 +138,25 @@ while not done:
                         pallette_index -= 1
                     else:
                         pallette_index = 3
-            elif key[pygame.K_UP]:
-                if background.pallettes[pallette_index][active - 1] < 63:
-                    background.pallettes[pallette_index][active - 1] += 1
+            elif key[pygame.K_UP] and ACTIVE:
+                if background.pallettes[pallette_index][ACTIVE - 1] < 63:
+                    background.pallettes[pallette_index][ACTIVE - 1] += 1
                 else:
-                    background.pallettes[pallette_index][active - 1] = 0
-                    
-            elif key[pygame.K_DOWN]:
-                if background.pallettes[pallette_index][active - 1] > 0:
-                    background.pallettes[pallette_index][active - 1] -= 1
+                    background.pallettes[pallette_index][ACTIVE - 1] = 0
+
+            elif key[pygame.K_DOWN] and ACTIVE:
+                if background.pallettes[pallette_index][ACTIVE - 1] > 0:
+                    background.pallettes[pallette_index][ACTIVE - 1] -= 1
                 else:
-                    background.pallettes[pallette_index][active - 1] = 63
+                    background.pallettes[pallette_index][ACTIVE - 1] = 63
+            ## Saving the background, CHR and pallette Data
             elif key[pygame.K_LCTRL] and key[pygame.K_s]:
                 background.save(input("Filename: "))
+            ## Opening a file
             elif key[pygame.K_LCTRL] and key[pygame.K_o]:
                 background.load(input("Filename: "))
-                
+
         clock.tick(30)
-    
+
 pygame.quit()
 os._exit(0)
